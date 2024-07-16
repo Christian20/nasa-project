2fb9b3282a59dc372a60226c5bb25583d6508e2e
Christian Steia <christian_steia@yahoo.com>
2024-07-16 17:11:06 +0300

    implemented /POST launches endpoint

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -33,7 +33,11 @@ function loadPlanetsData() {
   });
 }
 
+function getAllPlanets() {
+  return habitablePlanets;
+}
+
 module.exports = {
-  planets: habitablePlanets,
   loadPlanetsData,
+  getAllPlanets, 
 }
